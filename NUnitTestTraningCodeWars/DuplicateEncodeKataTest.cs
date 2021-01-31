b6e4f7058c45794f1a71d4891cb910180d64188f
denisiodrigues <denisio@ymail.com>
2021-01-30 22:59:06 -0300

    Uma resolução pra mudar de nível

---
+++ NUnitTestTraningCodeWars/DuplicateEncodeKataTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TraningCodeWars.Licao;
+
+namespace TraningCodeWars.Test
+{
+    [TestFixture]
+    public class DuplicateEncodeKataTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("(((", DuplicateEncodeKata.DuplicateEncode2("din"));
+            Assert.AreEqual("()()()", DuplicateEncodeKata.DuplicateEncode2("recede"));
+            Assert.AreEqual(")())())", DuplicateEncodeKata.DuplicateEncode2("Success"), "should ignore case");
+            Assert.AreEqual("))((", DuplicateEncodeKata.DuplicateEncode2("(( @"));
+        }
+    }
+}
---
+++ TraningCodeWars/Licao/DuplicateEncodeKata.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraningCodeWars.Licao
+{
+    public class DuplicateEncodeKata
+    {
+        public static string DuplicateEncode(string word)
+        {
+            var newWord = string.Empty;
+            foreach (var item in word.ToLower())
+            {
+                if (MoreOneCharacter(item, word.ToLower()))
+                {
+                    newWord += ")";
+                }
+                else
+                {
+                    newWord += "(";
+                }
+            }
+
+
+            return newWord;
+        }
+
+        public static bool MoreOneCharacter(char character, string word)
+        {
+            return word.Count(x => x.Equals(character)) > 1;
+        }
+
+        //Melhor solução do site
+        public static string DuplicateEncode2(string word)
+        {
+            return new string(word.ToLower()
+                .Select(
+                            ch => word.ToLower()
+                            .Count(innerCh => ch == innerCh) == 1 ? '(' : ')').ToArray()
+                        );
+        }
+    }
+}
